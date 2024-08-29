b0bdac8124bf6a48959c466fe656fe0a2acb81e2
roblesdotdev <httpsnip@gmail.com>
2024-08-29 18:15:47 -0300

    issue listing

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,10 +15,25 @@ export async function loader() {
 export default function Index() {
   const { issues } = useLoaderData<typeof loader>()
   return (
-    <div className="flex flex-col items-center justify-center p-8">
-      <h1 className="text-lg">Welcome to Remix on Vercel!</h1>
-      <div className="mt-6">
-        <pre>{JSON.stringify(issues, null, 2)}</pre>
+    <div className="container py-8">
+      <h1 className="text-lg">Recent issues</h1>
+      <div className="mt-6 max-w-screen-sm">
+        <ul className="flex flex-col gap-2 divide-y border">
+          {issues.map(issue => (
+            <li key={issue.id} className="px-4 py-3 text-sm">
+              <p className="line-clamp-2 text-neutral-400">
+                <span className="font-medium text-neutral-300">
+                  {issue.title}
+                </span>
+                {' | '}
+                {issue.description}
+              </p>
+              <p className="mt-2 text-xs">
+                <span className="text-neutral-400">by</span> {issue.author}
+              </p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,13 @@ import { type Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '1.5rem',
+      screens: {
+        '2xl': '65ch',
+      },
+    },
     extend: {},
   },
   plugins: [],
